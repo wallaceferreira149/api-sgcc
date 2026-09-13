f303b3476f9771a6239ec08df7aaca09e595bf44
wallaceferreira149 <wallaceferreira149@gmail.com>
2026-09-13 16:10:21 -0300

    :tada: build:  initial commit project started

---
+++ src/main/java/dev/arcanus/api_sgcc/ApiSgccApplication.java
@@ -0,0 +1,13 @@
+package dev.arcanus.api_sgcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSgccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSgccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/arcanus/api_sgcc/ApiSgccApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arcanus.api_sgcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSgccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
